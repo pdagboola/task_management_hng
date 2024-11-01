fb63b6db41342619301011f4aea540945f6e4300
Peter Agboola <pdagboola@gmail.com>
2024-11-02 00:19:23 +0100

    feat: lost learning JwtStrategy

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,11 +12,15 @@ tasks.get("/tasks/page/:pagenum", async (req, res) => {
   const { pagenum } = req.params;
   // let offset = 0;
   const offset = (pagenum - 1) * 5;
-  const allTasks = await getTasks(offset);
-  if (!allTasks || allTasks.length === 0) {
-    res.send(JSON.stringify({ message: "No tasks created yet" }));
-  } else {
-    res.send(JSON.stringify(allTasks));
+  try {
+    const allTasks = await getTasks(offset);
+    if (!allTasks || allTasks.length === 0) {
+      res.json({ sucess: true, message: "No tasks created yet" });
+    } else {
+      res.json({ message: allTasks });
+    }
+  } catch (err) {
+    res.json({ success: false, data: err.message });
   }
 });
 
@@ -24,29 +28,27 @@ tasks.post("/tasks", async (req, res) => {
   const { title, description, due_date, status } = req.body;
   console.log("task post request body:", req.body);
   const created_at = Date();
-  const err = await createTask(
-    title,
-    description,
-    due_date,
-    status,
-    created_at
-  );
-  console.log("error from line 34:", err);
-  if (err) {
-    res.send(JSON.stringify({ message: "Task could not be created" }));
-  } else {
-    res.send(JSON.stringify({ message: "Task created!" }));
+  try {
+    await createTask(title, description, due_date, status, created_at);
+    // console.log("error from line 34:", err);
+    res.json({ success: true, data: "Task created!" });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
   }
 });
 
 tasks.get("/tasks/:id", async (req, res) => {
   const { id } = req.params;
-  const task = await getTaskById(id);
-  console.log("here's the task object", task);
-  if (!task || task.length === 0) {
-    res.send(JSON.stringify({ message: "task not found" }));
-  } else {
-    res.send(JSON.stringify({ task }));
+  try {
+    const task = await getTaskById(id);
+    console.log("here's the task object", task);
+    if (!task || task.length === 0) {
+      res.json({ success: true, message: "Task not found" });
+    } else {
+      res.json({ success: true, message: task });
+    }
+  } catch (err) {
+    res.json({ success: false, message: err.message });
   }
 });
 
@@ -54,23 +56,35 @@ tasks.put("/tasks/:id", async (req, res) => {
   const { id } = req.params;
   const { title, description, due_date, status } = req.body;
   const updated_at = Date();
-  const task = await getTaskById(id);
-  // console.log(task);
-  await updateTaskById(
-    !title ? task.title : title,
-    !description ? task.description : description,
-    !due_date ? task.due_date : due_date,
-    !status ? task.status : status,
-    updated_at,
-    id
-  );
-  res.send(JSON.stringify({ message: "Task updated!" }));
+  try {
+    const task = await getTaskById(id);
+    if (!task || task.length === 0) {
+      res.json({ success: true, message: "Task not found" });
+    } else {
+      await updateTaskById(
+        !title ? task.title : title,
+        !description ? task.description : description,
+        !due_date ? task.due_date : due_date,
+        !status ? task.status : status,
+        updated_at,
+        id
+      );
+      res.json({ success: true, message: "Task updated!" });
+    }
+    // console.log(task);
+  } catch (err) {
+    res.json({ success: false, message: err.message });
+  }
 });
 
 tasks.delete("/tasks/:id", async (req, res) => {
   const { id } = req.params;
-  await deleteTAskById(id);
-  res.send(JSON.stringify({ message: "Task deleted!" }));
+  try {
+    await deleteTAskById(id);
+    res.json({ success: true, message: "Task deleted!" });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
+  }
 });
 
 module.exports = tasks;
--- models/populatedb.js
+++ models/populatedb.js
@@ -2,45 +2,31 @@ const pool = require("./pool");
 
 class Populate {
   async createTable() {
-    try {
-      console.log("...creating table");
-      await pool.query(`DROP TABLE IF EXISTS users`);
-      await pool.query(`DROP TABLE IF EXISTS tasks`);
-      await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
-      await pool.query(
-        `CREATE TABLE users(id uuid DEFAULT uuid_generate_v4() PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password BYTEA);`
-      );
-      await pool.query(
-        `CREATE TABLE tasks( id SERIAL PRIMARY KEY, title VARCHAR(255), description VARCHAR(255), due_date DATE, status VARCHAR(255), created_at VARCHAR, updated_at VARCHAR);`
-      );
-      console.log("table created");
-    } catch (err) {
-      return err;
-    }
+    console.log("...creating table");
+    await pool.query(`DROP TABLE IF EXISTS users`);
+    await pool.query(`DROP TABLE IF EXISTS tasks`);
+    await pool.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await pool.query(
+      `CREATE TABLE users(id uuid DEFAULT uuid_generate_v4() PRIMARY KEY, username VARCHAR(255), email VARCHAR(255), password BYTEA);`
+    );
+    await pool.query(
+      `CREATE TABLE tasks( id SERIAL PRIMARY KEY, title VARCHAR(255), description VARCHAR(255), due_date DATE, status VARCHAR(255), created_at VARCHAR, updated_at VARCHAR);`
+    );
+    console.log("table created");
   }
   async createUser(username, email, password) {
-    try {
-      await pool.query(
-        `INSERT INTO users(username, email, password) VALUES ($1, $2, $3);`,
-        [username, email, password]
-      );
-    } catch (error) {
-      throw err;
-    }
+    await pool.query(
+      `INSERT INTO users(username, email, password) VALUES ($1, $2, $3);`,
+      [username, email, password]
+    );
   }
   async createTask(title, description, due_date, status, created_at) {
-    try {
-      await pool.query(
-        `INSERT INTO tasks(title, description, due_date, status, created_at) VALUES ($1, $2, $3, $4, $5);`,
-        [title, description, due_date, status, created_at]
-      );
-    } catch (err) {
-      throw err;
-    }
+    await pool.query(
+      `INSERT INTO tasks(title, description, due_date, status, created_at) VALUES ($1, $2, $3, $4, $5);`,
+      [title, description, due_date, status, created_at]
+    );
   }
   async getTasks(offset) {
-    try {
-    } catch (err) {}
     const { rows } = await pool.query(
       `SELECT * FROM tasks ORDER BY id LIMIT 5 OFFSET $1;`,
       [offset]
@@ -59,7 +45,7 @@ class Populate {
       [title, description, due_date, status, updated_at, id]
     );
   }
-  async deleteTAskById(id) {
+  async deleteTaskById(id) {
     await pool.query(`DELETE FROM tasks WHERE id = $1`, [id]);
   }
 }
